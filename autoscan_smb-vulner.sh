df474fba2a18a88f6ac819366e35a04fd587698d
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2023-11-02 16:04:06 +0100

    Add files via upload

---
+++ autoscan_smb-vulner.sh
@@ -0,0 +1,5 @@
+map --script smb-enum-shares.nse -p445 $1
+nmap –script smb-enum-users.nse -p445 $1
+nmap --script smb-enum-domains.nse,smb-enum-groups.nse,smb-enum-processes.nse,smb-enum-services.nse,smb-enum-sessions.nse,smb-enum-shares.nse,smb-enum-users.nse -p445 $1
+nmap --script smb-vuln-conficker.nse,smb-vuln-cve2009-3103.nse,smb-vuln-cve-2017-7494.nse,smb-vuln-ms06-025.nse,smb-vuln-ms07-029.nse,smb-vuln-ms08-067.nse,smb-vuln-ms10-054.nse,smb-vuln-ms10-061.nse,smb-vuln-ms17-010.nse,smb-vuln-regsvc-dos.nse,smb-vuln-webexec.nse -p445 $1
+nmap --script smb-vuln-cve-2017-7494 --script-args smb-vuln-cve-2017-7494.check-version -p445 $1
