4f980582f0fec758bfb7cf84e35512ae1e725ede
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2023-11-02 15:15:20 +0100

    Add files via upload

---
+++ autoscan_nmap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+target="$1"
+ports=$(nmap -p- --min-rate 1000 "$target" | grep "^ *[0-9]" | grep "open" | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//')
+
+echo "Running second nmap scan with open ports: $ports"
+
+nmap -p "$ports" -sC -sV -A "$target"
